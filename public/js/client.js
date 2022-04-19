db42e6cae9c7008d849b3a44abf1bc75429c3d90
Lasse Aakjær <51378679+TheLoGgI@users.noreply.github.com>
2022-04-19 22:33:53 +0200

    update packages

--- public/js/client.js
+++ public/js/client.js
@@ -2,97 +2,90 @@
 
 //Fetching data from API (Application Programming Interface)
 async function postData(nr) {
-  const response = await fetch('/api/routes', {
-    method: 'POST',
-    mode: 'cors',
+  const response = await fetch("/api/routes", {
+    method: "POST",
+    mode: "cors",
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      route: nr
-    })
+      route: nr,
+    }),
   })
-
 
   const data = await response.text()
   const appendArray = await cleanData(data)
   removeAllChildNodes()
   appendNode(appendArray)
-  return
 }
 
 // Data Handler
 function cleanData(data) {
   const cleanArray = []
-  data.split('"').forEach(index => {
+  data.split('"').forEach((index) => {
     let duplicated = cleanArray.includes(index)
-    if (index != '[' && index != ']' && index != ',' && !duplicated) {
+    if (index != "[" && index != "]" && index != "," && !duplicated) {
       cleanArray.push(index)
     }
   })
   return cleanArray
 }
 
-
 //Adds the hole route to the DOM (Document Object Model)
 function appendNode(nodeList) {
-  const addressList = document.querySelector('#address-list')
+  const addressList = document.querySelector("#address-list")
 
   //Create new li element and append
-  nodeList.forEach(item => {
-    let li = document.createElement("li");
+  nodeList.forEach((item) => {
+    let li = document.createElement("li")
     li.classList = "list-item"
     // li.setAttribute("draggable", "true")
-    li.append(item);
+    li.append(item)
     ul.appendChild(li)
   })
-
 }
 
 function removeAllChildNodes() {
-const nodeList = document.querySelectorAll('#address-list .list-item')
-nodeList.forEach(element => {
-  element.remove()
-})
-
+  const nodeList = document.querySelectorAll("#address-list .list-item")
+  nodeList.forEach((element) => {
+    element.remove()
+  })
 }
 
 function quickSortRoute(data) {
-  const domList = document.querySelectorAll('#address-list .list-item')
+  const domList = document.querySelectorAll("#address-list .list-item")
 
   // Sorting Elements
-  data.then(lookupArray => {
-    const nodeArray = []
-    const cloneNodes = []
-    // Adding number attribute to dom elemnts & pushing elemtns to nodeArray
-    for (let i = 0; i < domList.length; i++) {
-      let currentNode = domList[i].textContent.split(',')[0] //Checking node of place
+  data
+    .then((lookupArray) => {
+      const nodeArray = []
+      const cloneNodes = []
+      // Adding number attribute to dom elemnts & pushing elemtns to nodeArray
+      for (let i = 0; i < domList.length; i++) {
+        let currentNode = domList[i].textContent.split(",")[0] //Checking node of place
 
-      domList[i].setAttribute("number", lookupArray.indexOf(currentNode))
-      nodeArray.push(domList[i])
-    }
+        domList[i].setAttribute("number", lookupArray.indexOf(currentNode))
+        nodeArray.push(domList[i])
+      }
 
-    //QuickSorting elemnts in array
-    nodeArray.sort((a, b) => a.attributes[2].nodeValue - b.attributes[2].nodeValue)
+      //QuickSorting elemnts in array
+      nodeArray.sort(
+        (a, b) => a.attributes[2].nodeValue - b.attributes[2].nodeValue
+      )
 
-    // Cloning textContent of elements to cloneNodes
-    nodeArray.forEach(item => {
-      cloneNodes.push(item.textContent)
+      // Cloning textContent of elements to cloneNodes
+      nodeArray.forEach((item) => {
+        cloneNodes.push(item.textContent)
+      })
+
+      // changing DOM elemnts textContent
+      for (let index = 0; index < domList.length; index++) {
+        domList[index].textContent = cloneNodes[index]
+        domList[index].removeAttribute("number")
+      }
     })
-
-    // changing DOM elemnts textContent
-    for (let index = 0; index < domList.length; index++) {
-      domList[index].textContent = cloneNodes[index]
-      domList[index].removeAttribute("number")
-    }
-
-
-  }).catch(error => {
-    throw error
-  })
-
-
-
-
+    .catch((error) => {
+      throw error
+    })
 }
